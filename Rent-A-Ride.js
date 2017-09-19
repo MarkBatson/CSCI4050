0b916464b16fbd47c684694e4500442b17d3476c
Mark Batson <markdbatson@gmail.com>
2017-09-19 18:16:07 -0400

    UI Update
    Added feedback page. Updated homepage. Made index more responsive. Added more javascript.

--- Rent-A-Ride.js
+++ Rent-A-Ride.js
@@ -1,7 +1,29 @@
 var menuButton = document.getElementById("menu");
 menuButton.addEventListener("click", expandMenu);
-
 function expandMenu() {
-  console.log("here")
   document.getElementById("drop-down-menu").style.display = "inherit";
+}
+
+var pickUpButton = document.getElementById("pick-up-button");
+pickUpButton.addEventListener("click", pickedUp);
+function pickedUp() {
+  if (pickUpButton.textContent == "pick up") {
+    pickUpButton.textContent = "return";
+  }
+  else {
+    document.getElementById("example-car").style.display = "none";
+  }
+}
+
+var findButton = document.getElementById("find-button");
+findButton.addEventListener("click", search);
+function search() {
+  document.getElementById("car-search").style.display = "inherit";
+}
+
+var rentButton = document.getElementById("rent-button");
+rentButton.addEventListener("click", rent);
+function rent() {
+  document.getElementById("example-car").style.display = "flex";
+  pickUpButton.textContent = "pick up";
 }
